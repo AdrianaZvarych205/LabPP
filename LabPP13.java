54ba32c4d57e9cf5ba920495ca0ff352effb694e
AdrianaZvarych205 <74185814+AdrianaZvarych205@users.noreply.github.com>
2020-11-09 19:51:35 +0200

    Add files via upload

---
+++ LabPP13.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class LabPP13 {
+
+        public static void main(String[] args)
+        {
+            System.out.println("Введіть три аргументи:\n");
+            for (int i = 0; i < 3; i++)
+            {
+                System.out.println(args[i]);
+            }
+        }
+}
