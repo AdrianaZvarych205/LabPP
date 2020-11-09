cabfc6d09f53f1eba3216873aed2bdf22d0cb43e
AdrianaZvarych205 <74185814+AdrianaZvarych205@users.noreply.github.com>
2020-11-09 16:28:56 +0200

    Add files via upload

---
+++ LabaPP12.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class LabaPP12 {
+    public static int i;
+    public static char ch;
+
+    public static void main(String[] args) {
+	    System.out.println("Значення int= " + i);
+        System.out.println("Значення char= " + ch);
+    }
+}
