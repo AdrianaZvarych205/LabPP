27c0ee36e2f73d4c5c01025742c98352f6e9961e
AdrianaZvarych205 <74185814+AdrianaZvarych205@users.noreply.github.com>
2020-11-27 14:33:07 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,23 +1,48 @@
-package com.butterfly.task;
+package com.butterfly.main;
 
-import java.util.Scanner;
+import com.butterfly.product.Product;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    final static Scanner sc = new Scanner(System.in);
+
+    static List<Product> byName(List<Product> products, String naming) {
+        Predicate<Product> name = product -> product.getNaming().equals(naming);
+        return products.stream().filter(name).collect(Collectors.toList());
+    }
+
+    static List<Product> byNameAndPrice(List<Product> products, String naming, double maxPrice) {
+        Predicate<Product> nameAndPrice = product -> product.getNaming().equals(naming) && product.getPrice() <= maxPrice;
+        return products.stream().filter(nameAndPrice).collect(Collectors.toList());
+    }
+
+    static List<Product> byExpirationPeriod(List<Product> products, Date minExpirationPeriod) {
+        Predicate<Product> name = product -> product.getExpirationPeriod().compareTo(minExpirationPeriod) >= 0;
+        return products.stream().filter(name).collect(Collectors.toList());
+    }
+
+    static void display(List<Product> products, String label) {
+        System.out.println(label);
+        for (var product : products) {
+            System.out.println(product.toString());
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args) {
-        int left, right, n;
-        System.out.println("Enter bounds and fibo length: ");
-        left = sc.nextInt();
-        right = sc.nextInt();
-        n = sc.nextInt();
 
-        LabPP14 task = new LabPP14();
-        task.inputArray(left, right);
-        task.display();
-        task.displaySums();
-        task.buildFib(n);
-        task.getFiboPercentage();
+        List<Product> products = new ArrayList<Product>();
+        products.add(new Product(1, "A", "B", 1, new Date(1, 0, 0), 1));
+        products.add(new Product(2, "A", "C", 2, new Date(2, 0, 0), 1));
+        products.add(new Product(3, "B", "C", 3, new Date(3, 0, 0), 1));
+
+        display(byName(products, "A"), "by name");
+        display(byNameAndPrice(products, "A", 1.5), "by name and price");
+        display(byExpirationPeriod(products, new Date(1, 6, 0)), "by expiration period");
     }
 }
